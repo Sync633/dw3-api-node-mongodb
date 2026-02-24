24827bc54235a92744deda746367af78e2aaa51a
DSM3 <fatec-dsm3@academico.intra>
2026-02-24 20:15:40 -0300

    Aula 01.2 - Desenvolvendo uma API Rest com Node.js

---
+++ aula01-criando-a-api/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose from "mongoose";
+import Game from "./models/Games.js";
+
+
+const app = express();
+// Configurando o Express
+app.use(express.json()) // Permite o uso de JSON na aplicação
+
+// Inciando a conexão com o banco de dados MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/api-the-games")
+
+// Rota Principal
+app.get("/", (req, res) => {
+    const games = [
+        {
+            title: "Overwatch",
+            year: "2022",
+            plataform: "PC",
+            price : 0
+        },
+        {
+            title: "Titanfall 2",
+            year: "2016",
+            plataform: "PC",
+            price: 30
+        }
+    ]
+    res.status(200).json(games)
+});
+
+// Rodando a API na porta 4000
+const port = 4000;
+app.listen(port, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(`API Rodando em http://localhost:${port}`);
+    }
+})
